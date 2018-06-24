68a06a8da871b440680466fffa2978d60a8ecb0b
Gene Boggs <gene.boggs@gmail.com>
2018-06-23 21:30:50 -0700

    Cleanup code

--- next-soccer.js
+++ next-soccer.js
@@ -1,23 +1,20 @@
-// phantomjs scrape.js
+// phantomjs next-soccer.js
 
-//var address = 'http://www.moe.gov.na/st_li_institutions.php';
 var address = 'http://www.sportstats.com/soccer/matches/';
+var path = 'next-soccer.html'
+
+var fs = require('fs');
 
 var webPage = require('webpage');
 var page = webPage.create();
 
-var fs = require('fs');
-var path = 'scraped.html'
-
 page.open(address, function (status) {
     if (status !== 'success') {
         console.log('Unable to load the address!');
-        phantom.exit();
     } else {
-//        window.setTimeout(function () {
-            var content = page.content;
-            fs.write(path,content,'w')
-            phantom.exit();
-//        }, 1000); // Change timeout as required to allow sufficient time 
+        var content = page.content;
+        fs.write(path,content,'w');
     }
+
+    phantom.exit();
 });
