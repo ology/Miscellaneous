3c45da43f974c1bc3cb0886770d5ca601e425272
Gene Boggs <gene@ology.net>
2020-05-23 16:33:43 -0700

    Set the days on page load

--- mojo-date-widget.pl
+++ mojo-date-widget.pl
@@ -50,12 +50,14 @@ __DATA__
 <select name="day" id="day">
 </select>
 <script>
-function leapyear(year) {
-    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
-}
-var nums = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31};
 jQuery(function($) {
-    $('select[name="year"], select[name="month"]').change(function () {
+    function leapyear(year) {
+        return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+    }
+
+    var nums = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31};
+
+    function setDays() {
         var year = $('select[name="year"]').val();
         if (leapyear(year)) {
             nums[2] = 29;
@@ -68,6 +70,12 @@ jQuery(function($) {
         for (i = 1; i <= nums[$('select[name="month"]').val()]; i++) {
             $('select[name="day"]').append('<option value="' + i + '">' + i + '</option>');
         }
+    }
+
+    setDays();
+
+    $('select[name="year"], select[name="month"]').change(function () {
+        setDays();
     });
 });
 </script>
