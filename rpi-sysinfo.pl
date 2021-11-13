70b8ea6d444a2aa8ae1a20469ec1b8aeedd00680
Gene Boggs <gene@ology.net>
2021-11-13 02:15:29 -0800

    Add gpio system functions

--- rpi-sysinfo.pl
+++ rpi-sysinfo.pl
@@ -4,7 +4,7 @@ use warnings;
 
 use RPi::SysInfo ();
 
-my $type  = shift || die "Usage: perl $0 [conf|net|fs|pi|stat|pins]\n";
+my $type  = shift || die "Usage: perl $0 [conf|net|fs|pi|pins|stat|gpiov|gpiora]\n";
 my $first = shift // 0;
 my $last  = shift // 53;
  
@@ -15,12 +15,14 @@ my %dispatch = (
   net  => sub { $sys->network_info },
   fs   => sub { $sys->file_system },
   pi   => sub { $sys->pi_details },
+  pins => sub { $sys->gpio_info([ $first .. $last ]) },
   stat => sub {
     return join(' ', 'CPU:', $sys->cpu_percent, "%\n")
          . join(' ', 'MEM:', $sys->mem_percent, "%\n")
          . join(' ', 'Temp:', $sys->core_temp, "C°\n")
   },
-  pins => sub { $sys->gpio_info([ $first .. $last ]) },
+  gpiov  => sub { qx{ gpio -v } },
+  gpiora => sub { qx{ gpio readall } },
 );
 
 print $dispatch{$type}->();
