ae56f9d55237a366a227a91e3b8fbe95cec7cf3e
Gene Boggs <gene@ology.net>
2022-12-20 16:05:52 -0800

    Fix test indent

--- g_app.pm
+++ g_app.pm
@@ -163,7 +163,10 @@ use Test::More;
 use Test::Mojo;
 
 my $t = Test::Mojo->new('<%= $class %>');
-$t->get_ok('/')->status_is(200)->content_like(qr/Mojolicious/i);
+$t->get_ok('/')
+  ->status_is(200)
+  ->content_like(qr/Mojolicious/i)
+;
 
 done_testing();
 
