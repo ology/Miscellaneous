95c505c8ec5ec37af04125f4f5dc65830bb29503
Gene Boggs <gene@ology.net>
2024-05-09 19:51:37 -0700

    Whitespace!

--- mojo-harvest-testers.pl
+++ mojo-harvest-testers.pl
@@ -11,7 +11,6 @@ my $url = "$site/author/$author";
 
 my $ua = Mojo::UserAgent->new;
 my $tx = $ua->get($url);
-
 my $content = $tx->res->body;
 my $dom = Mojo::DOM->new($content);
 
@@ -19,12 +18,16 @@ my $links = $dom->find('a.release-name');
 
 for my $link (@$links) {
     $url = $site . $link->attr('href');
+
     $tx = $ua->get($url);
     $content = $tx->res->body;
     $dom = Mojo::DOM->new($content);
+
     my $testers = $dom->at('li a[title=Matrix]')->parent;
     (my $text = $testers->all_text) =~ s/\n//g;
     $text =~ s/\s+/ /g;
+
     print $link->all_text, ' :', $text, "\n";
+
     sleep 1;
 }
