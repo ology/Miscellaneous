da12218bf40343047458168a6913443861e79932
Gene Boggs <gene.boggs@gmail.com>
2026-09-22 09:55:47 -0700

    I

--- favorites.pl
+++ favorites.pl
@@ -10,14 +10,13 @@ my $mcpan    = MetaCPAN::Client->new;
 my $author   = $mcpan->author($name);
 my $releases = $author->releases;
 
-my ($i, $total, $sum) = (0, 0, 0);
+my ($i, $sum) = (0, 0);
 
 while (my $rel = $releases->next) {
     $i++;
-    $total++;
     my $fav = $mcpan->favorite({ distribution => $rel->distribution });
     say $i, '. ', $rel->distribution, ' (v', $rel->version, ') ', 'Favorites=', $fav->total;
     $sum += $fav->total;
 }
 
-say "\nTotal dists: $total, Favs: $sum";
+say "\nTotal dists: $i, Favs: $sum";
